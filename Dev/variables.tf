7f789ee3085bfa5ff590caea6b15cc763dc94e4c
MozkaGit <mozkaops@gmail.com>
2023-09-23 21:56:06 +0200

    Add key-pairs and private-key variables to Dev env

--- Dev/variables.tf
+++ Dev/variables.tf
@@ -1,9 +1,17 @@
 variable "instancetype" {
-  default = "t2.nano"
+  default = "t2.micro"
 }
 
 variable "aws_name_tag" {
   default = {
     Name = "ec2-dev-mozka"
   }
+}
+
+variable "key-pairs" {
+  default = "devops-mozka"
+}
+
+variable "private-key" {
+  default = "./devops-mozka.pem"
 }
